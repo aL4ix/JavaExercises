e17c42e7e3318a4f8eac637629a73467c6535133
aLaix <alan.limagalvez@yahoo.com>
2021-03-28 23:34:47 -0700

    First two of the second chapter

---
+++ src/Ch02/Ex02/Ch02Ex02.java
@@ -0,0 +1,13 @@
+package Ch02.Ex02;
+
+public class Ch02Ex02 {
+    public static void main(String[] args) {
+        System.out.println("Ch02Ex02");
+        /*
+        Scanner.nextInt() is mutator because it changes the state of the object,
+        the documentation says "If the translation is successful, the scanner advances past the input that matched."
+        As for Random.nextInt() it is a mutator because it's is changing the state of the object too,
+        It needs to change the seed to calculate a new random number.
+         */
+    }
+}
